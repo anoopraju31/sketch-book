69229551c2af9ab1d578c6f8ec4ff04152dd6aee
anoopraju31 <anoopraju31@gmail.com>
2023-11-22 23:12:49 +0530

    Menu Slice created added to the store

--- src/app/redux/menuSlice.ts
+++ src/app/redux/menuSlice.ts
@@ -1,10 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { MENU_ITEMS } from '../utills/constants'
+
+interface InitialState {
+	activeMenuItem: 'PENCIL' | 'ERASER' | 'UNDO' | 'REDO' | 'DOWNLOAD'
+	actionMenuItem: null | string
+}
+
+const initialState: InitialState = {
+	// @ts-ignore
+	activeMenuItem: MENU_ITEMS.PENCIL,
+	actionMenuItem: null,
+}
 
 const menuSlice = createSlice({
 	name: 'menu',
-	initialState: {},
-	reducers: {},
+	initialState,
+	reducers: {
+		menuItemClick: (state, action) => {
+			state.activeMenuItem = action.payload
+		},
+		actionItemClick: (state, action) => {
+			state.actionMenuItem = action.payload
+		},
+	},
 })
 
-export const {} = menuSlice.actions
+export const { menuItemClick, actionItemClick } = menuSlice.actions
 export default menuSlice.reducer
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import menuReducer from './menuSlice'
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		menu: menuReducer,
+	},
 })
 
 export type RootState = ReturnType<typeof store.getState>
