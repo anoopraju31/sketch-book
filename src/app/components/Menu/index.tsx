c88b13d23ac7477fbc87307bc69fb8f7e9ca0067
anoopraju31 <anoopraju31@gmail.com>
2023-11-22 23:19:24 +0530

    Menu Component - Click Event Handler added

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -1,25 +1,34 @@
+import { useAppDispatch } from '@/app/hooks/reduxHook'
 import { FaEraser, FaFileDownload, FaPencilAlt } from 'react-icons/fa'
 import { FaRotateLeft, FaRotateRight } from 'react-icons/fa6'
 import Icon from './Icon'
+import { MENU_ITEMS } from '@/app/utills/constants'
+
 import styles from './index.module.css'
+import { menuItemClick } from '@/app/redux/menuSlice'
 
 const Menu = () => {
+	const dispatch = useAppDispatch()
+	const handeClick = (itemName: string) => {
+		dispatch(menuItemClick(itemName))
+	}
+
 	return (
 		<div className={styles.menuContainer}>
 			<Icon>
-				<FaPencilAlt />
+				<FaPencilAlt onClick={() => handeClick(MENU_ITEMS.PENCIL)} />
 			</Icon>
 			<Icon>
-				<FaEraser />
+				<FaEraser onClick={() => handeClick(MENU_ITEMS.ERASER)} />
 			</Icon>
 			<Icon>
-				<FaRotateLeft />
+				<FaRotateLeft onClick={() => handeClick(MENU_ITEMS.UNDO)} />
 			</Icon>
 			<Icon>
-				<FaRotateRight />
+				<FaRotateRight onClick={() => handeClick(MENU_ITEMS.REDO)} />
 			</Icon>
 			<Icon>
-				<FaFileDownload />
+				<FaFileDownload onClick={() => handeClick(MENU_ITEMS.DOWNLOAD)} />
 			</Icon>
 		</div>
 	)
