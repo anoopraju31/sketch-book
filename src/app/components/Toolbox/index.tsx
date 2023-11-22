410af1ab534da308eaeb6669b17deb9531a965c9
anoopraju31 <anoopraju31@gmail.com>
2023-11-22 21:24:27 +0530

    Toolbox Component - layout added

--- src/app/components/Toolbox/index.tsx
+++ src/app/components/Toolbox/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
 const Toolbox = () => {
-	return <div>Toolbox</div>
+	const updateBrushSize = () => {}
+
+	return (
+		<div>
+			<div className=''>
+				<h4> Stroke Color </h4>
+
+				<div className=''>
+					<div />
+				</div>
+			</div>
+
+			<div className=''>
+				<h4> Brush Size </h4>
+
+				<div className=''>
+					<input
+						type='range'
+						min={1}
+						max={10}
+						step={1}
+						title='bursh size'
+						onChange={updateBrushSize}
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Toolbox
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from './components/Menu'
+import Toolbox from './components/Toolbox'
 
 const HomePage = () => {
 	return (
 		<div>
 			<Menu />
+			<Toolbox />
 		</div>
 	)
 }
