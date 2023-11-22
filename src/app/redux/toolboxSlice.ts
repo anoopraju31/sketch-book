10a2802fc1117f0c9c6f4e97bc7123d17613bdad
anoopraju31 <anoopraju31@gmail.com>
2023-11-22 22:24:38 +0530

    Tool Box Slice - initial

---
+++ src/app/redux/toolboxSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const toolboxSlice = createSlice({
+	name: 'toolbox',
+	initialState: {},
+	reducers: {},
+})
+
+export const {} = toolboxSlice.actions
+export default toolboxSlice.reducer
