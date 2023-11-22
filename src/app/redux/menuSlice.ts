8f50843f159e99f4fe5a404f955a38e4fa602c8f
anoopraju31 <anoopraju31@gmail.com>
2023-11-22 22:24:51 +0530

    Menu Slice - initial

---
+++ src/app/redux/menuSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const menuSlice = createSlice({
+	name: 'menu',
+	initialState: {},
+	reducers: {},
+})
+
+export const {} = menuSlice.actions
+export default menuSlice.reducer
