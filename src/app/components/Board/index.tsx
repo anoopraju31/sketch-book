2a168985031cab46e1eb8cedaf011446188eb81c
anoopraju31 <anoopraju31@gmail.com>
2023-11-23 20:14:36 +0530

    Board Component - size & color added

--- src/app/components/Board/index.tsx
+++ src/app/components/Board/index.tsx
@@ -1,9 +1,29 @@
 'use client'
 
 import { useEffect, useRef } from 'react'
+import { useAppSelector } from '@/app/hooks/reduxHook'
+import { COLORS, MENU_ITEMS } from '@/app/utills/constants'
 
 const Board = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const activeMenuItem = useAppSelector((state) => state.menu.activeMenuItem)
+	const pencilColor = useAppSelector((state) => state.toolbox.PENCIL.color)
+	const pencilSize = useAppSelector((state) => state.toolbox.PENCIL.size)
+	const eraserColor = useAppSelector((state) => state.toolbox.ERASER.color)
+	const eraserSize = useAppSelector((state) => state.toolbox.ERASER.size)
+	const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
+	const showBrushToolOption =
+		activeMenuItem === MENU_ITEMS.PENCIL || activeMenuItem === MENU_ITEMS.ERASER
+	const size = showBrushToolOption
+		? showStrokeToolOption
+			? pencilSize
+			: eraserSize
+		: 0
+	const color = showBrushToolOption
+		? showStrokeToolOption
+			? pencilColor
+			: eraserColor
+		: COLORS.WHITE
 
 	useEffect(() => {
 		if (!canvasRef.current) return
@@ -15,6 +35,8 @@ const Board = () => {
 		canvas.width = window.innerWidth
 		canvas.height = window.innerHeight
 	}, [])
+
+	console.log(color, size)
 
 	return <canvas ref={canvasRef}></canvas>
 }
