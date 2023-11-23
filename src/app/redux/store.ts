3a65dfd614710d76bd7732832528e2840801907c
anoopraju31 <anoopraju31@gmail.com>
2023-11-23 19:10:00 +0530

    Tool Box Slice - created & added to store

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import menuReducer from './menuSlice'
+import toolboxReducer from './toolboxSlice'
 
 export const store = configureStore({
 	reducer: {
 		menu: menuReducer,
+		toolbox: toolboxReducer,
 	},
 })
 
--- src/app/redux/toolboxSlice.ts
+++ src/app/redux/toolboxSlice.ts
@@ -1,10 +1,64 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { COLORS, Color, MENU_ITEMS, MenuItem } from '../utills/constants'
+
+interface InitialState {
+	[MENU_ITEMS.PENCIL]: {
+		color: Color
+		size: number
+	}
+	[MENU_ITEMS.ERASER]: {
+		color: Color
+		size: number
+	}
+	[MENU_ITEMS.UNDO]: {}
+	[MENU_ITEMS.REDO]: {}
+	[MENU_ITEMS.DOWNLOAD]: {}
+}
+
+const initialState: InitialState = {
+	[MENU_ITEMS.PENCIL]: {
+		color: COLORS.BLACK,
+		size: 3,
+	},
+	[MENU_ITEMS.ERASER]: {
+		color: COLORS.WHITE,
+		size: 3,
+	},
+	[MENU_ITEMS.UNDO]: {},
+	[MENU_ITEMS.REDO]: {},
+	[MENU_ITEMS.DOWNLOAD]: {},
+}
 
 const toolboxSlice = createSlice({
 	name: 'toolbox',
-	initialState: {},
-	reducers: {},
+	initialState,
+	reducers: {
+		changeColor: (
+			state,
+			action: PayloadAction<{
+				item: typeof MENU_ITEMS.PENCIL | typeof MENU_ITEMS.ERASER
+				color: Color
+			}>,
+		) => {
+			const { item, color } = action.payload
+
+			if (item === MENU_ITEMS.PENCIL || item === MENU_ITEMS.ERASER)
+				state[item].color = color
+		},
+		changeBrushSize: (
+			state,
+			action: PayloadAction<{
+				item: typeof MENU_ITEMS.PENCIL | typeof MENU_ITEMS.ERASER
+				size: number
+			}>,
+		) => {
+			const { item, size } = action.payload
+
+			if (item === MENU_ITEMS.PENCIL || item === MENU_ITEMS.ERASER)
+				state[item].size = size
+		},
+	},
 })
 
-export const {} = toolboxSlice.actions
+export const { changeColor, changeBrushSize } = toolboxSlice.actions
 export default toolboxSlice.reducer
