41a922d41d7c5f3e866dcea37ceb9d5fa86fe07b
anoopraju31 <anoopraju31@gmail.com>
2023-11-30 11:26:36 +0530

    Download Sketch functionality refactor

--- src/app/components/Board/index.tsx
+++ src/app/components/Board/index.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useEffect, useLayoutEffect, useRef } from 'react'
-import { useAppSelector } from '@/app/hooks/reduxHook'
+import { useAppDispatch, useAppSelector } from '@/app/hooks/reduxHook'
 import { COLORS, MENU_ITEMS } from '@/app/utills/constants'
+import { actionItemClick } from '@/app/redux/menuSlice'
 
 const Board = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null)
@@ -12,6 +13,8 @@ const Board = () => {
 	const pencilColor = useAppSelector((state) => state.toolbox.PENCIL.color)
 	const pencilSize = useAppSelector((state) => state.toolbox.PENCIL.size)
 	const eraserSize = useAppSelector((state) => state.toolbox.ERASER.size)
+	const dispatch = useAppDispatch()
+
 	const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
 	const showBrushToolOption =
 		activeMenuItem === MENU_ITEMS.PENCIL || activeMenuItem === MENU_ITEMS.ERASER
@@ -35,7 +38,9 @@ const Board = () => {
 			anchor.download = 'sketch.png'
 			anchor.click()
 		}
-	}, [actionMenuItem])
+
+		dispatch(actionItemClick(null))
+	}, [actionMenuItem, dispatch])
 
 	useEffect(() => {
 		if (!canvasRef.current) return
